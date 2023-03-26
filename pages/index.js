61ba193437d04e13a2537dd1735075814716df72
Joshua Mangiola <joshm982@gmail.com>
2023-03-26 22:37:48 +1030

    SEO Changes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Button, Content } from "@ripe-ui/react";
 import { Header } from "../components/header";
 import { Hero } from "@ripe-ui/react";
 import { useRouter } from "next/navigation";
+import Head from "next/head";
 import { FormattedMessage, useIntl } from "react-intl";
 
 export default function Home() {
@@ -9,11 +10,18 @@ export default function Home() {
 
   const intl = useIntl();
 
+  const pageTitle = intl.formatMessage({ id: "page.home.title" });
+  const pageDesc = intl.formatMessage({ id: "page.home.desc" });
+
   const title = intl.formatMessage({ id: "page.home.hero.title" });
   const subtitle = intl.formatMessage({ id: "page.home.hero.title" });
 
   return (
     <div>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDesc} />
+      </Head>
       <Header showMobileMenu={false} />
       <Content>
         <Hero title={title} subtitle={subtitle} color="white">
